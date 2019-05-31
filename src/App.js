a02f95aaf73ebf179d0909c1365eba4a8dbab2f8
veroena <veronica.enamorado@gmail.com>
2019-05-31 16:58:37 +0200

    Employee map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const employeeArr = [
+  {
+      "givenName": "Pedro Javier",
+      "sn": "Perez Mora",
+      "sAMAccountName": "pedro.perez"
+  },
+  {
+      "givenName": "Francisco Javier",
+      "sn": "Pérez García",
+      "sAMAccountName": "javier.perezgarcia"
+  },
+  {
+      "givenName": "Javier",
+      "sn": "Perez Alonso",
+      "sAMAccountName": "javier.perez"
+  },
+  {
+      "givenName": "Javier",
+      "sn": "Perez Gonzalo",
+      "sAMAccountName": "javier.pgonzalo"
+  },
+  {
+      "givenName": "Juan Javier",
+      "sn": "Perez Ruiz",
+      "sAMAccountName": "juan.perez"
+  },
+  {
+      "givenName": "Javier",
+      "sn": "Perez Garcia",
+      "sAMAccountName": "jperez"
+  }
+]
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <ul className="employee__list">
+          {employeeArr
+            .map(item => 
+              <li className="employee__list--item">{item.givenName} {item.sn}</li>
+              )
+          }  
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
