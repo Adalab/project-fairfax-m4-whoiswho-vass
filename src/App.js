d9dc96893a798f46c047cf7ba98cc1bd7e99182c
AranchaAdalab <aranzazunarvaez@gmail.com>
2019-05-31 18:39:21 +0200

    filter name

--- src/App.js
+++ src/App.js
@@ -3,34 +3,34 @@ import './App.css';
 
 const employeeArr = [
   {
-      "givenName": "Pedro Javier",
-      "sn": "Perez Mora",
-      "sAMAccountName": "pedro.perez"
+    "givenName": "Pedro Javier",
+    "sn": "Perez Mora",
+    "sAMAccountName": "pedro.perez"
   },
   {
-      "givenName": "Francisco Javier",
-      "sn": "Pérez García",
-      "sAMAccountName": "javier.perezgarcia"
+    "givenName": "Francisco Javier",
+    "sn": "Pérez García",
+    "sAMAccountName": "javier.perezgarcia"
   },
   {
-      "givenName": "Javier",
-      "sn": "Perez Alonso",
-      "sAMAccountName": "javier.perez"
+    "givenName": "Javier",
+    "sn": "Perez Alonso",
+    "sAMAccountName": "javier.perez"
   },
   {
-      "givenName": "Javier",
-      "sn": "Perez Gonzalo",
-      "sAMAccountName": "javier.pgonzalo"
+    "givenName": "Javier",
+    "sn": "Perez Gonzalo",
+    "sAMAccountName": "javier.pgonzalo"
   },
   {
-      "givenName": "Juan Javier",
-      "sn": "Perez Ruiz",
-      "sAMAccountName": "juan.perez"
+    "givenName": "Juan Javier",
+    "sn": "Perez Ruiz",
+    "sAMAccountName": "juan.perez"
   },
   {
-      "givenName": "Javier",
-      "sn": "Perez Garcia",
-      "sAMAccountName": "jperez"
+    "givenName": "Javier",
+    "sn": "Perez Garcia",
+    "sAMAccountName": "jperez"
   }
 ]
 
@@ -39,30 +39,46 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      nameArr: [],
       filterName: '',
       filterSn: ''
     }
+
     this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  componentDidMount() {
+    const newArr = employeeArr.map((item, index) => {
+      return { ...item, id: `employeeId-${index}`, fullName: `${item.givenName} ${item.sn}` };
+    });
+    console.log(newArr);
+    this.setState({nameArr:newArr});
   }
 
   handleFilter(event) {
     const currentValue = event.currentTarget.value;
-    this.setState({filterName : currentValue});
+    this.setState({ filterName: currentValue });
   }
 
   render() {
+    const { nameArr, filterName } = this.state;
     return (
       <div className="App">
         <label htmlFor="filterEmployee">Campo de búsqueda</label>
-        <input onChange={this.handleFilter} name="filterEmployee" id="filterEmployee" type="text"/>
+        <input onChange={this.handleFilter} name="filterEmployee" id="filterEmployee" type="text" />
         <ul className="employee__list">
-          {employeeArr 
-              .filter(item => item.givenName.includes(this.state.filterName))
-              // .filter(item => item.sn.includes(this.state.filterName))
-              .map(item => 
-                <li className="employee__list--item">{item.givenName} {item.sn}</li>
-                )
-          }  
+          {/* {employeeArr
+            .filter(item => item.givenName.includes(filterName) || item.sn.includes(filterName))
+            .map(item =>
+              <li className="employee__list--item">{item.givenName} {item.sn}</li>
+            )
+          } */}
+          {nameArr
+            .filter(item => item.fullName.includes(filterName))
+            .map(item =>
+              <li className="employee__list--item">{item.fullName}</li>
+            )
+          }
         </ul>
       </div>
     );
