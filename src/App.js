f22c52fd22904a5da7a73018e74a33ee6d5d987e
anajd <ana.jaime.diaz@gmail.com>
2019-06-01 12:00:49 +0200

    filter name and last name

--- src/App.js
+++ src/App.js
@@ -3,56 +3,54 @@ import './App.css';
 
 const employeeArr = [
   {
-    "givenName": "Pedro Javier",
-    "sn": "Perez Mora",
-    "sAMAccountName": "pedro.perez"
+    givenName: 'Pedro Javier',
+    sn: 'Perez Mora',
+    sAMAccountName: 'pedro.perez'
   },
   {
-    "givenName": "Francisco Javier",
-    "sn": "Pérez García",
-    "sAMAccountName": "javier.perezgarcia"
+    givenName: 'Francisco Javier',
+    sn: 'Pérez García',
+    sAMAccountName: 'javier.perezgarcia'
   },
   {
-    "givenName": "Javier",
-    "sn": "Perez Alonso",
-    "sAMAccountName": "javier.perez"
+    givenName: 'Javier',
+    sn: 'Perez Alonso',
+    sAMAccountName: 'javier.perez'
   },
   {
-    "givenName": "Javier",
-    "sn": "Perez Gonzalo",
-    "sAMAccountName": "javier.pgonzalo"
+    givenName: 'Javier',
+    sn: 'Perez Gonzalo',
+    sAMAccountName: 'javier.pgonzalo'
   },
   {
-    "givenName": "Juan Javier",
-    "sn": "Perez Ruiz",
-    "sAMAccountName": "juan.perez"
+    givenName: 'Juan Javier',
+    sn: 'Perez Ruiz',
+    sAMAccountName: 'juan.perez'
   },
   {
-    "givenName": "Javier",
-    "sn": "Perez Garcia",
-    "sAMAccountName": "jperez"
+    givenName: 'Javier',
+    sn: 'Perez Garcia',
+    sAMAccountName: 'jperez'
   }
-]
+];
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       nameArr: [],
-      filterName: '',
-      filterSn: ''
-    }
+      filterName: ''
+    };
 
     this.handleFilter = this.handleFilter.bind(this);
   }
 
   componentDidMount() {
-    const newArr = employeeArr.map((item, index) => {
-      return { ...item, id: `employeeId-${index}`, fullName: `${item.givenName} ${item.sn}` };
+    const newArr = employeeArr.map(item => {
+      return { ...item };
     });
     console.log(newArr);
-    this.setState({nameArr:newArr});
+    this.setState({ nameArr: newArr });
   }
 
   handleFilter(event) {
@@ -65,20 +63,20 @@ class App extends React.Component {
     return (
       <div className="App">
         <label htmlFor="filterEmployee">Campo de búsqueda</label>
-        <input onChange={this.handleFilter} name="filterEmployee" id="filterEmployee" type="text" />
+        <input
+          onChange={this.handleFilter}
+          name="filterEmployee"
+          id="filterEmployee"
+          type="text"
+        />
         <ul className="employee__list">
-          {/* {employeeArr
-            .filter(item => item.givenName.includes(filterName) || item.sn.includes(filterName))
-            .map(item =>
-              <li className="employee__list--item">{item.givenName} {item.sn}</li>
-            )
-          } */}
           {nameArr
-            .filter(item => item.fullName.includes(filterName))
-            .map(item =>
-              <li className="employee__list--item">{item.fullName}</li>
-            )
-          }
+            .filter(item => `${item.givenName} ${item.sn}`.includes(filterName))
+            .map(item => (
+              <li className="employee__list--item">
+                {item.givenName} {item.sn}
+              </li>
+            ))}
         </ul>
       </div>
     );
