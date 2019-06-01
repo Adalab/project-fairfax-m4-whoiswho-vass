f90545c18e80b28b17ac2634c7a0e050b8ad0950
anajd <ana.jaime.diaz@gmail.com>
2019-06-01 18:21:53 +0200

    visible filter

--- src/App.js
+++ src/App.js
@@ -62,37 +62,67 @@ class App extends React.Component {
   handleCollapsible(event) {
     const newCollapsibleId = event.currentTarget.id;
     this.setState(prevState => {
-      if(parseInt(newCollapsibleId) === prevState.collapsibleId) {
-        return {collapsibleId: null}
+      if (parseInt(newCollapsibleId) === prevState.collapsibleId) {
+        return { collapsibleId: null };
       } else {
-        return {collapsibleId: parseInt(newCollapsibleId)}
+        return { collapsibleId: parseInt(newCollapsibleId) };
       }
-    })
+    });
   }
 
   render() {
     const { nameArr, filterName, collapsibleId } = this.state;
-    return (
-      <div className="App">
-        <label htmlFor="filterEmployee">Campo de búsqueda</label>
-        <input
-          onChange={this.handleFilter}
-          name="filterEmployee"
-          id="filterEmployee"
-          type="text"
-        />
-        <ul className="employee__list">
-          {nameArr
-            .filter(item => `${item.givenName} ${item.sn}`.toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "").includes(filterName.toLocaleLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")))
-            .map(item => (
-              <li id={item.id} key={item.id} className="employee__list--item" onClick={this.handleCollapsible}>
-                {item.givenName} {item.sn}
-                {collapsibleId === item.id ? <p>Hola</p> : null}
-              </li>
-            ))}
-        </ul>
-      </div>
-    );
+    if (filterName === '') {
+      return (
+        <div className="input__container">
+          <label htmlFor="filterEmployee">Campo de búsqueda</label>
+          <input
+            onChange={this.handleFilter}
+            name="filterEmployee"
+            id="filterEmployee"
+            type="text"
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div className="App">
+          <label htmlFor="filterEmployee">Campo de búsqueda</label>
+          <input
+            onChange={this.handleFilter}
+            name="filterEmployee"
+            id="filterEmployee"
+            type="text"
+          />
+          <ul className="employee__list">
+            {nameArr
+              .filter(item =>
+                `${item.givenName} ${item.sn}`
+                  .toLocaleLowerCase()
+                  .normalize('NFD')
+                  .replace(/[\u0300-\u036f]/g, '')
+                  .includes(
+                    filterName
+                      .toLocaleLowerCase()
+                      .normalize('NFD')
+                      .replace(/[\u0300-\u036f]/g, '')
+                  )
+              )
+              .map(item => (
+                <li
+                  id={item.id}
+                  key={item.id}
+                  className="employee__list--item"
+                  onClick={this.handleCollapsible}
+                >
+                  {item.givenName} {item.sn}
+                  {collapsibleId === item.id ? <p>Hola</p> : null}
+                </li>
+              ))}
+          </ul>
+        </div>
+      );
+    }
   }
 }
 
