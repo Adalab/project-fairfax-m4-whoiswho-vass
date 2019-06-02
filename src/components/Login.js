d0051913f4d1e8fb3bdc0e3685b7e7c43eb7c4db
anajd <ana.jaime.diaz@gmail.com>
2019-06-02 18:00:32 +0200

    Add Lara's changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,30 +1,24 @@
 import React from 'react';
-
+import './Login.scss';
+​
+​
 class Login extends React.Component {
-  render() {
-    const { changePassword, passwordState } = this.props;
-    return (
-      <React.Fragment>
-        <h1 className="login__logo">Aquí va el logo</h1>
-        <div className="inputs__container">
-          <label htmlFor="input__user" className="input__user">
-            Usuario
-          </label>
-          <input type="text" id="input__user" className="input__user" />
-          <label htmlFor="input__password" className="input__password">
-            Contraseña
-          </label>
-          <input
-            type={passwordState}
-            id="input__password"
-            className="input__password"
-          />
-          <i class="fas fa-eye" onClick={changePassword} />
-          <input type="submit" value="Entrar" className="input__submit" />
-        </div>
-      </React.Fragment>
-    );
-  }
+ render() {
+  const {changePassword, passwordState} = this.props;
+  return(
+    <div className="login__container">
+     <img src="https://www.vass.es/wp-content/uploads/2018/04/logotipo-VASS.png" alt="logotipo VASS" className="logo__img"/>
+     <div className="inputs__container">
+      <label htmlFor="input__user" className="label__item label__user">Usuario</label>
+      <input type="text" id="input__user" className="input__item input__user"/>
+      <label htmlFor="input__password" className="label__item label__password">Contraseña</label>
+      <input type={passwordState} id="input__password" className="input__item input__password"/>
+      <i class="fas fa-eye" onClick={changePassword}></i>
+      <input type="submit" value="Entrar" className="input__submit"/>
+     </div>
+    </div>
+  );
+ }
 }
-
+​
 export default Login;
