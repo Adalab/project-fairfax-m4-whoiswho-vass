904c40d9ea9db9202e9b0300599733fcf236f389
lararb <lararb91@gmail.com>
2019-06-02 17:47:54 +0200

    Finish mobile version styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Login.scss';
 
+
 class Login extends React.Component {
   render() {
     const {changePassword, passwordState} = this.props;
@@ -8,10 +9,10 @@ class Login extends React.Component {
         <div className="login__container">
           <img src="https://www.vass.es/wp-content/uploads/2018/04/logotipo-VASS.png" alt="logotipo VASS" className="logo__img"/>
           <div className="inputs__container">
-            <label htmlFor="input__user" className="input__user">Usuario</label>
-            <input type="text" id="input__user" className="input__user"/>
-            <label htmlFor="input__password" className="input__password">Contraseña</label>
-            <input type={passwordState} id="input__password" className="input__password"/>
+            <label htmlFor="input__user" className="label__item label__user">Usuario</label>
+            <input type="text" id="input__user" className="input__item input__user"/>
+            <label htmlFor="input__password" className="label__item label__password">Contraseña</label>
+            <input type={passwordState} id="input__password" className="input__item input__password"/>
             <i class="fas fa-eye" onClick={changePassword}></i>
             <input type="submit" value="Entrar" className="input__submit"/>
           </div>
